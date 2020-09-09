a96b35c88d328a7960ec495da6b59e9607877c2c
Vladres <vladres121@gmail.com>
2020-09-09 19:53:22 +0300

    add front end listener + add new messege

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js'
+import axios from './axios'
 
 function App() {
+  const [messeges,SetMesseges] = useState([])
+
+  useEffect(() => {
+    axios.get('/messeges/sync')
+      .then((res) => {
+          SetMesseges(res.data)
+    })
+  }, []);
+
+ useEffect(() => {
+  const pusher = new Pusher('bf5cef76b093dddf0b91', {
+    cluster: 'eu'
+  });
+
+  const channel = pusher.subscribe('messeges');
+  channel.bind('inserted', function(data) {
+    SetMesseges([...messeges,data]);
+  });
+
+  return () => {
+    channel.unbind_all();
+    channel.unsubscribe();
+  };
+ }, [messeges]);
+
+ console.log(messeges)
+
+
   return (
     <div className="App">
       <div className="app-body">
         <Sidebar />
 
-        <Chat />
+        <Chat messeges={messeges}/>
       </div>
     </div>
   );
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Chat.css"
 import { Avatar, IconButton } from '@material-ui/core';
 import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons';
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
+import axios from './axios'
 
-function Chat() {
+function Chat({ messeges }) {
+
+    const [input, setInput] = useState('');
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messeges/new', {
+            messege: input,
+            name: "Novod",
+            timestamp: "JUST now",
+            recieved: true
+        }).catch((err)=>{
+            console.log(err)
+        })
+
+        setInput('');
+    }
+
     return (
         <div className="chat">
 
@@ -31,37 +48,28 @@ function Chat() {
             </div>
 
             <div className="chat-body">
-                <p className="chat-message">
-                    <span className="chat-name">Vlad</span>
-                    This is a message
-                    <span className="chat-timestamp">
-                        {new Date().toISOString()}
-                    </span>
-                </p>
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Vlad</span>
-                    This is a message
-                    <span className="chat-timestamp">
-                        {new Date().toISOString()}
-                    </span>
-                </p>
+                {messeges.map((messege) => {
+                    return <p className={`chat-message ${messege.recieved && "chat-reciever"}`}>
+                        <span className="chat-name">{messege.name}</span>
+                        {messege.messege}
+                        <span className="chat-timestamp">
+                            {messege.timestamp}
+                        </span>
+                    </p>
+                })}
             </div>
 
             <div className="chat-footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input 
-                    // value={input}
-                    // onChange={(e) => setInput(e.target.value)}
-                    placeholder="Type a message..."
-                    type="text"
+                    <input
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type a message..."
+                        type="text"
                     />
 
-                    <button type="submit" 
-                    // onClick={sendMessage}
-                    >
-                        Send a message
-                    </button>
+                    <button type="submit" onClick={sendMessage}>Send a message</button>
                 </form>
                 <MicIcon />
             </div>
